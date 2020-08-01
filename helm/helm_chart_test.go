93c6c1bf03ff256012a60c9a29cf7c64e0790c8d
faustoespinal <faustoespinal@gmail.com>
2020-08-01 17:07:01 -0500

    Unit test update

--- helm/helm_chart_test.go
+++ helm/helm_chart_test.go
@@ -1,14 +1,33 @@
 package helm
 
 import (
+	"latimer/core"
 	"testing"
-
-	"github.com/golang/glog"
 )
 
-func Test_helm_chart_String(t *testing.T) {
-	t.Run("helm_load", func(t *testing.T) {
-		t.Logf("testing helm load")
+const (
+	MemChartRef = "stable/memcached"
+)
+
+var chartDescriptor core.ChartDescriptor = core.ChartDescriptor{
+	Name:         "sample-chart",
+	ChartName:    "sample-chart",
+	ChartLocator: "stable/memcached",
+	Namespace:    "paas",
+	ReleaseName:  "test-memcached",
+	Values: []struct {
+		URL string `json:"url"`
+	}{},
+}
+
+func Test_helmchart(t *testing.T) {
+	t.Run("helm-chart-create", func(t *testing.T) {
+		t.Logf("Testing helm chart create")
+
+		chart := NewChart(&chartDescriptor)
+		if chart.Name != chartDescriptor.Name {
+			t.Errorf("Chart creation failed %v  descriptor=%v", chart, chartDescriptor.Name)
+		}
+		t.Logf("Created helm chart: %v\n", chart)
 	})
-	glog.Info("test helm")
 }
